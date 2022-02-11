5f3df8792465afae082c117266396c13c4bcf91c
Lekshmiiii <lekshmis748@gmail.com>
2022-02-11 09:31:11 +0530

    front_end_ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import ErrorPage from './components/error/Error'
+import Header from './components/header/Header';
+import ArticleList from './components/article/ArticleList';
+import Article from './components/article/Article';
+import AddPost from './components/post/AddPost';
+import Signup from './components/userAccounts/Signup';
+import Login from './components/userAccounts/Login';
+import useToken from './components/userAccounts/useToken';
+import Logout from './components/userAccounts/Logout';
+import UpdatePost from './components/post/UpdatePost';
+import DeletePost from './components/post/DeletePost';
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return (
+      <Router>
+        <Header token={ token }/>
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/login" element={<Login setToken={setToken} />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    ) 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header token={ token } />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/article-list" element={<ArticleList token={ token }/>} />
+        <Route path="/article/:id" element={<Article token={ token } />} />
+        <Route path="/post/create" element={<AddPost />} />
+        <Route path="/post/update/:id" element={<UpdatePost />} />
+        <Route path="/post/delete/:id" element={<DeletePost />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+    
   );
 }
 
